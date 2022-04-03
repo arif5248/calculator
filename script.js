41c12de5b459020d770df79193894531fba9375e
arif5248 <arifislam11ctg@gmail.com>
2022-04-04 04:57:27 +0600

    my clcutor

---
+++ script.js
@@ -0,0 +1,11 @@
+function btnFunction(val){
+    form.display.value +=val;
+
+}
+function equal(){
+    form.display.value=eval(form.display.value);
+}
+
+function clrScreen(){
+    form.display.value="";
+}
